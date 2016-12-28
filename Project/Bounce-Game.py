fbdb6fe2221b363c31dc8cddd3ea30283f8c57aa
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2016-12-28 01:32:39 -0500

    Bounce-Game - Added Paddle and its functioning

--- Project/Bounce-Game.py
+++ Project/Bounce-Game.py
@@ -33,7 +33,7 @@ class Ball:
 		self.canvas.move(self.id, self.x, self.y)
 		#coords return an array with [x1, y1, x2, y2]
 		pos = self.canvas.coords(self.id)	#we are getting the current coordinates
-		print(pos)
+		#print(pos)
 		if pos[1] <= 0:
 			self.y = 3
 		if pos[3] >= self.canvas_height:
@@ -41,15 +41,39 @@ class Ball:
 		if pos[0] <= 0:
 			self.x =3 
 		if pos[2] >= self.canvas_width:
-			self.x = -3 
+			self.x = -3
+
+class Paddle: 
+	def __init__(self, canvas, color):
+		self.canvas = canvas
+		self.id = canvas.create_rectangle(0, 0, 100, 10, fill = color)
+		self.canvas.move(self.id, 200, 300)
+		self.x = 0
+		self.canvas_width = self.canvas.winfo_width()
+		self.canvas.bind_all('<KeyPress-Left>', self.left)
+		self.canvas.bind_all('<KeyPress-Right>', self.right)
+
+	def draw(self):
+		self.canvas.move(self.id, self.x, 0)
+		pos = self.canvas.coords(self.id)
+		if pos[0] <= 0:
+			self.x = 0
+		if pos[2] >= self.canvas_width:
+			self.x = 0 
+
+	def left(self, evt):
+		self.x = -2
+
+	def right(self, evt):
+		self.x = 2
+
 
 ball = Ball(canvas, 'red')
-
-
-
+paddle = Paddle(canvas, 'gray')
 #Loop to keep on updating the canvas, to always show the ball moving
 while 1:
 	ball.draw()
+	paddle.draw()
 	tk.update_idletasks()
 	tk.update()
 	time.sleep(0.01)
