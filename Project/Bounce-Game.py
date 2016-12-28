127873b2e6819016f162504db41a88cbc116ad09
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2016-12-28 01:18:22 -0500

    Bounce-Game - Bpuncing feature of the ball

--- Project/Bounce-Game.py
+++ Project/Bounce-Game.py
@@ -20,11 +20,28 @@ class Ball:
 		self.canvas = canvas
 		self.id = canvas.create_oval(10, 10, 25, 25, fill = color)
 		self.canvas.move(self.id, 245, 100)
+		start = [-3, -2, -1, 0, 1, 2, 3]
+		random.shuffle(start)
+		self.x = start[0]
+		self.y = -5
+		self.canvas_height = self.canvas.winfo_height()
+		self.canvas_width = self.canvas.winfo_width()	
 
 	#function for animation
 	def draw(self):
 		#move function takes 3 arguements, !st = id, 2nd = how much you wanna move, 3rd = movement up or down
-		self.canvas.move(self.id, 0, -1	)
+		self.canvas.move(self.id, self.x, self.y)
+		#coords return an array with [x1, y1, x2, y2]
+		pos = self.canvas.coords(self.id)	#we are getting the current coordinates
+		print(pos)
+		if pos[1] <= 0:
+			self.y = 3
+		if pos[3] >= self.canvas_height:
+			self.y = -3
+		if pos[0] <= 0:
+			self.x =3 
+		if pos[2] >= self.canvas_width:
+			self.x = -3 
 
 ball = Ball(canvas, 'red')
 
