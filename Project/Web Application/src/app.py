2a93bc04d062b0be6a1951319d751e7c2d6a6f22
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-01-04 23:43:34 -0500

    Added front end

--- Project/Web Application/src/app.py
+++ Project/Web Application/src/app.py
@@ -2,24 +2,60 @@ __author__ = "Arjun"
 
 from flask import Flask, render_template, request, session
 from src.models.user import User
+from src.common.database import Database
 
 # telling python that we are creating a flask application
 app = Flask(__name__)   # built in variable
+app.secret_key = "Arjun"
 
 # creating the API's end point
 @app.route('/')
-def hello():
-    return render_template('login.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.before_first_request
+def initialize():
+    Database.initialize()
+
+@app.route('/auth/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
 
     if User.login_valid(email, password):
         User.login(email)
+    else:
+        session['email'] = None
 
-    return render_template("profile.html", email="hello")
+    return render_template("profile.html", email=session['email'])
+
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
+    return render_template("profile.html", email=session['email'])
+
+@app.route('/blogs/<string:user_id>')
+@app.route('/blogs')
+def user_blogs(user_id=None):
+    if user_id is not None:
+        user = User.get_by_id(user_id)
+    else:
+        user = User.get_by_email(session['email'])
+    blogs = user.get_blogs()
+
+    return render_template("user_blogs.html", blogs= blogs, email=user.email)
 
 
 if __name__ == '__main__':
