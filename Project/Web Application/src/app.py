a83d215e957535a3b112f4e9ad55b72d5c527b48
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-01-06 23:35:55 -0500

    The complete web application

--- Project/Web Application/src/app.py
+++ Project/Web Application/src/app.py
@@ -1,9 +1,12 @@
+from flask.helpers import make_response
+
 __author__ = "Arjun"
 
 from flask import Flask, render_template, request, session
 from src.models.user import User
 from src.common.database import Database
-
+from src.models.blog import Blog
+from src.models.post import Post
 # telling python that we are creating a flask application
 app = Flask(__name__)   # built in variable
 app.secret_key = "Arjun"
@@ -57,6 +60,40 @@ def user_blogs(user_id=None):
 
     return render_template("user_blogs.html", blogs= blogs, email=user.email)
 
+@app.route('/posts/<string:blog_id>')
+def blog_posts(blog_id):
+    blog = Blog.from_mongo(blog_id)
+    posts = blog.get_posts()
+
+    return render_template('posts.html', posts = posts, blog_title = blog.title, blog_id= blog._id)
+
+@app.route('/blogs/new', methods=['POST', 'GET'])
+def create_new_blogs():
+    if request.method == 'GET':
+        return render_template('new_blog.html')
+    else:
+        title = request.form['title']
+        description = request.form['description']
+        user = User.get_by_email(session['email'])
+
+        new_blog = Blog(user.email, title, description, user._id)
+        new_blog.save_to_mongo()
+
+        return make_response(user_blogs(user._id))
+
+@app.route('/posts/new/<string:blog_id>', methods=['POST', 'GET'])
+def create_new_post(blog_id):
+    if request.method == 'GET':
+        return render_template('new_post.html')
+    else:
+        title = request.form['title']
+        content = request.form['content']
+        user = User.get_by_email(session['email'])
+
+        new_post = Post(blog_id, title, content, user.email)
+        new_post.save_to_mongo()
+
+        return make_response(blog_posts(blog_id))
 
 if __name__ == '__main__':
     app.run(port=4994)
